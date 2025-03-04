9b435fe7d3dbe0de8d097c917a8b2bb0d99c1b27
yogesh249 <yogesh249@gmail.com>
2025-03-04 15:48:46 +0530

    fixed project name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'yogesh249', // Usually your GitHub org/user name.
-  projectName: 'mywebsite', // Usually your repo name. 
+  projectName: 'week7-mywebsite', // Usually your repo name. 
 
   onBrokenLinks: 'throw', 
   onBrokenMarkdownLinks: 'warn',
